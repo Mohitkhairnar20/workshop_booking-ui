66412025c727dc7ed6681d3abbeb19c5bbc70cc4
Mohit Khairnar <mohitkhairnar20@gmail.com>
2026-04-08 19:29:53 +0530

    Add cms migrations for fresh local setup

--- workshop_portal/views.py
+++ workshop_portal/views.py
@@ -2,15 +2,20 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.conf import settings
+from django.db.utils import OperationalError, ProgrammingError
 
 # Local Imports
 from cms.models import Page
 
 
 def index(request):
-    page = Page.objects.filter(title=settings.HOME_PAGE_TITLE)
-    if page.exists():
-        redirect_url = reverse("cms:home", args=[page.first().permalink])
-    else:
+    try:
+        page = Page.objects.filter(title=settings.HOME_PAGE_TITLE)
+        if page.exists():
+            redirect_url = reverse("cms:home", args=[page.first().permalink])
+        else:
+            redirect_url = reverse("workshop_app:index")
+    except (OperationalError, ProgrammingError):
+        # Fresh local setups may not have CMS tables yet.
         redirect_url = reverse("workshop_app:index")
     return redirect(redirect_url)
